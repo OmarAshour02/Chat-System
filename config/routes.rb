fea0fe491712931139dac70885e64bd58d5aad41
omarashour <omarashour02@gmail.com>
2024-09-15 15:47:47 +0300

    feat: add index to show all applications

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :applications, param: :token, only: [:create, :show, :update] do
+      resources :applications, param: :token, only: [:create, :show, :update, :index] do
         resources :chats, param: :number, only: [:create, :index, :show] do
           resources :messages, only: [:create, :index] do
             collection do
